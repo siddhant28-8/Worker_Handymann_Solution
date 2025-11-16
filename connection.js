2349709c68c8e6210e80f5ebf54f8f693e9f8710
siddhant28-8 <sid28rai8@gmail.com>
2025-11-16 17:48:48 +0530

    Connection chnages for 505 Error

--- connection.js
+++ connection.js
@@ -1,13 +1,24 @@
 // db.js
 const mongoose = require("mongoose");
 
+let isConnected=false;
+
 const connectDB = async () => {
+    if(isConnected){
+        return; //reuse existing connection as vercel uses make many instances
+    }
+
+    if(!process.env.MONGODB_URI){
+        throw new Error("Missing MongoDB connection string (MONGODB_URI)");
+    }
+
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
+        const db = await mongoose.connect(process.env.MONGODB_URI);
+        isConnected = db.connections[0].readyState === 1;
+        console.log("MongoDB Connected ");
     } catch (err) {
         console.error("MongoDB connection error:", err);
-        process.exit(1); // Exit the process if database connection fails
+        throw err; // Exit the process if database connection fails
     }
 };
 
